ed049ad4ba0ee86951a8bf575a2aff0f49b0a6bc
Megan Comeau <mlcomeau99@gmail.com>
2020-04-21 21:35:48 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match (array)
+    matches = []
+    word_array = word.split("")
+    array.each do |some_word|
+      temp_array = some_word.split("")
+      if temp_array.sort == word_array.sort
+        matches << some_word
+      end
+    end
+    matches
+  end
+
+end
